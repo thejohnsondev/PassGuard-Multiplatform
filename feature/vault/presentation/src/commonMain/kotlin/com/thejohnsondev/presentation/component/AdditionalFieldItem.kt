18466a32c81b57d0be0365ac39e38703d38d60b3
Andrew <thejohnsondev@gmail.com>
2025-01-18 11:51:02 +0200

    fix(vault): fix hiding keyboard on ime action on additional field value

--- feature/vault/presentation/src/commonMain/kotlin/com/thejohnsondev/presentation/component/AdditionalFieldItem.kt
+++ feature/vault/presentation/src/commonMain/kotlin/com/thejohnsondev/presentation/component/AdditionalFieldItem.kt
@@ -37,6 +37,7 @@ import com.thejohnsondev.ui.designsystem.Size2
 import com.thejohnsondev.ui.designsystem.Size4
 import com.thejohnsondev.ui.designsystem.Size8
 import com.thejohnsondev.ui.designsystem.Size98
+import com.thejohnsondev.ui.utils.KeyboardManager
 import org.jetbrains.compose.resources.stringResource
 import vaultmultiplatform.feature.vault.presentation.generated.resources.Res
 import vaultmultiplatform.feature.vault.presentation.generated.resources.title
@@ -62,6 +63,7 @@ fun AdditionalFieldItem(
     var isHidden by remember {
         mutableStateOf(false)
     }
+    val keyboardController = KeyboardManager.getKeyboardController()
     val eyeImage = if (isHidden) Icons.Filled.VisibilityOff else Icons.Filled.Visibility
     Surface(
         modifier = modifier
@@ -123,7 +125,7 @@ fun AdditionalFieldItem(
                                 passwordVisible = !isHidden,
                                 hint = stringResource(Res.string.value),
                                 onKeyboardAction = {
-                                    // TODO add function for closing the keyboard on iOS and Android
+                                    keyboardController?.hide()
                                 }
                             )
                         }
