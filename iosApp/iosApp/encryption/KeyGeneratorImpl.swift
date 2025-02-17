a3376e9bdf68607ffcc2a9307ce4dea000bab640
Andrew <thejohnsondev@gmail.com>
2025-02-17 18:12:59 +0200

    feat(ios): add secret key generation

--- iosApp/iosApp/encryption/KeyGeneratorImpl.swift
+++ iosApp/iosApp/encryption/KeyGeneratorImpl.swift
@@ -1,11 +1,21 @@
 import Foundation
 import ComposeApp
+import Security
 
 class KeyGeneratorImpl: NSObject, KeyGenerator {
     
     func generateSecretKey() -> KotlinByteArray {
-        return KotlinByteArray(size: 2) { index in
-            return index == 0 ? 0x00 : 0x01 // TODO for testing, replace with implementation
+        let keySize: Int32 = 32 // 256-bit AES key
+        var keyData = [UInt8](repeating: 0, count: Int(keySize))
+        
+        let result = SecRandomCopyBytes(kSecRandomDefault, Int(keySize), &keyData)
+        
+        if result == errSecSuccess {
+            return KotlinByteArray(size: keySize) { index in
+                return KotlinByte(value: Int8(bitPattern: keyData[Int(truncating: index)]))
+            }
+        } else {
+            fatalError("Failed to generate secure random key")
         }
     }
     
