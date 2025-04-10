a30ee11ce0568ad8d01d3df2dcea084bdd1b3afc
Andrew <thejohnsondev@gmail.com>
2025-04-10 18:05:10 +0300

    feat(tools): add main functionality in PasswordGeneratorViewModel

--- feature/tools/presentation/src/commonMain/kotlin/com/thejohnsondev/presentation/passwordgenerator/PasswordGeneratorViewModel.kt
+++ feature/tools/presentation/src/commonMain/kotlin/com/thejohnsondev/presentation/passwordgenerator/PasswordGeneratorViewModel.kt
@@ -7,7 +7,9 @@ import com.thejohnsondev.domain.GetPasswordGeneratorConfigUseCase
 import com.thejohnsondev.domain.UpdatePasswordGeneratorConfigUseCase
 import com.thejohnsondev.model.ScreenState
 import com.thejohnsondev.model.tools.PASSWORD_GENERATOR_DEFAULT_LENGTH
+import com.thejohnsondev.model.tools.PasswordGeneratedResult
 import com.thejohnsondev.model.tools.PasswordGenerationType
+import com.thejohnsondev.model.tools.PasswordGeneratorConfig
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
@@ -34,7 +36,7 @@ class PasswordGeneratorViewModel(
         when (action) {
             is Action.FetchConfig -> fetchConfig()
             is Action.Copy -> copy()
-            is Action.GeneratePassword -> generatePassword()
+            is Action.GeneratePassword -> launch { generatePassword() }
             is Action.Reset -> reset()
             is Action.UpdateIncludeDigits -> updateIncludeDigits(action.includeDigits)
             is Action.UpdateIncludeLower -> updateIncludeLower(action.includeLower)
@@ -45,44 +47,95 @@ class PasswordGeneratorViewModel(
         }
     }
 
-    private fun fetchConfig() {
-
+    private fun fetchConfig() = launch {
+        val config = getPasswordGeneratorConfigUseCase()
+        _state.value = state.value.copy(
+            length = config.length,
+            includeLower = config.includeLower,
+            includeUpper = config.includeUpper,
+            includeDigits = config.includeDigits,
+            includeSpecial = config.includeSpecial,
+            type = config.type
+        )
+        generatePassword()
     }
+
 
     private fun copy() {
-
+        // TODO implement
     }
 
-    private fun generatePassword() {
-
+    private suspend fun generatePassword() {
+        val result = generatePasswordUseCase(
+            PasswordGeneratorConfig(
+                type = state.value.type,
+                length = state.value.length,
+                includeLower = state.value.includeLower,
+                includeUpper = state.value.includeUpper,
+                includeDigits = state.value.includeDigits,
+                includeSpecial = state.value.includeSpecial
+            )
+        )
+        _state.value = state.value.copy(passwordGeneratedResult = result)
     }
 
-    private fun reset() {
-
+    private suspend fun updateConfig() {
+        updatePasswordGeneratorConfigUseCase(
+            type = state.value.type,
+            length = state.value.length,
+            includeLower = state.value.includeLower,
+            includeUpper = state.value.includeUpper,
+            includeDigits = state.value.includeDigits,
+            includeSpecial = state.value.includeSpecial
+        )
     }
 
-    private fun updateIncludeDigits(include: Boolean) {
-
+    private fun reset() = launch {
+        _state.value = state.value.copy(
+            length = PASSWORD_GENERATOR_DEFAULT_LENGTH,
+            includeLower = true,
+            includeUpper = true,
+            includeDigits = true,
+            includeSpecial = true,
+            type = PasswordGenerationType.RANDOM
+        )
+        generatePassword()
     }
 
-    private fun updateIncludeLower(include: Boolean) {
-
+    private fun updateIncludeDigits(include: Boolean) = launch {
+        _state.value = state.value.copy(includeDigits = include)
+        updateConfig()
+        generatePassword()
     }
 
-    private fun updateIncludeSpecial(include: Boolean) {
-
+    private fun updateIncludeLower(include: Boolean) = launch {
+        _state.value = state.value.copy(includeLower = include)
+        updateConfig()
+        generatePassword()
     }
 
-    private fun updateIncludeUpper(include: Boolean) {
-
+    private fun updateIncludeSpecial(include: Boolean) = launch {
+        _state.value = state.value.copy(includeSpecial = include)
+        updateConfig()
+        generatePassword()
     }
 
-    private fun updateLength(length: Int) {
-
+    private fun updateIncludeUpper(include: Boolean) = launch {
+        _state.value = state.value.copy(includeUpper = include)
+        updateConfig()
+        generatePassword()
     }
 
-    private fun updateType(type: PasswordGenerationType) {
+    private fun updateLength(length: Int) = launch {
+        _state.value = state.value.copy(length = length)
+        updateConfig()
+        generatePassword()
+    }
 
+    private fun updateType(type: PasswordGenerationType) = launch {
+        _state.value = state.value.copy(type = type)
+        updateConfig()
+        generatePassword()
     }
 
 
@@ -101,7 +154,7 @@ class PasswordGeneratorViewModel(
 
     data class State(
         val screenState: ScreenState = ScreenState.ShowContent,
-        val password: String = "",
+        val passwordGeneratedResult: PasswordGeneratedResult? = null,
         val length: Int = PASSWORD_GENERATOR_DEFAULT_LENGTH,
         val includeLower: Boolean = true,
         val includeUpper: Boolean = true,
