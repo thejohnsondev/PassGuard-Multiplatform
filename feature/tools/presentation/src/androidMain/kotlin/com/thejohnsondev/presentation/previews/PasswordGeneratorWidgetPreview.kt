8b13b1e9ab95be57d9ba0c3ba791560f186fcae1
Andrew <thejohnsondev@gmail.com>
2025-04-12 13:01:30 +0300

    feat(tools): add PasswordGeneratorWidget

--- feature/tools/presentation/src/androidMain/kotlin/com/thejohnsondev/presentation/previews/PasswordGeneratorWidgetPreview.kt
+++ feature/tools/presentation/src/androidMain/kotlin/com/thejohnsondev/presentation/previews/PasswordGeneratorWidgetPreview.kt
@@ -1,15 +1,59 @@
 package com.thejohnsondev.presentation.previews
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.thejohnsondev.model.tools.PasswordGeneratedResult
 import com.thejohnsondev.presentation.passwordgenerator.PasswordGeneratorViewModel
 import com.thejohnsondev.presentation.passwordgenerator.PasswordGeneratorWidgetContent
+import com.thejohnsondev.ui.designsystem.colorscheme.VaultDefaultTheme
 
 @Preview(showBackground = true)
 @Composable
 fun PasswordGeneratorWidgetPreview() {
-    PasswordGeneratorWidgetContent(
-        state = PasswordGeneratorViewModel.State(),
-        onAction = {}
-    )
+    VaultDefaultTheme(
+        dynamicColor = false,
+        darkTheme = true,
+        deviceThemeConfig = null
+    ) {
+        Surface(
+            color = MaterialTheme.colorScheme.surface
+        ) {
+            PasswordGeneratorWidgetContent(
+                modifier = Modifier.fillMaxWidth(),
+                state = PasswordGeneratorViewModel.State(),
+                onAction = {}
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun PasswordGeneratorWidgetGeneratedPreview() {
+    VaultDefaultTheme(
+        dynamicColor = false,
+        darkTheme = true,
+        deviceThemeConfig = null
+    ) {
+        Surface(
+            color = MaterialTheme.colorScheme.surface
+        ) {
+            PasswordGeneratorWidgetContent(
+                modifier = Modifier.fillMaxWidth(),
+                state = PasswordGeneratorViewModel.State(
+                    passwordGeneratedResult = PasswordGeneratedResult(
+                        password = "Qbegfdjl3lk49@#",
+                        strengthLevel = 6,
+                        suggestion = "Make a stronger password",
+                    )
+                ),
+                onAction = {}
+            )
+        }
+    }
 }
--- feature/tools/presentation/src/commonMain/kotlin/com/thejohnsondev/presentation/passwordgenerator/PasswordGeneratorWidget.kt
+++ feature/tools/presentation/src/commonMain/kotlin/com/thejohnsondev/presentation/passwordgenerator/PasswordGeneratorWidget.kt
@@ -1,10 +1,40 @@
 package com.thejohnsondev.presentation.passwordgenerator
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Casino
+import androidx.compose.material.icons.filled.CopyAll
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.hapticfeedback.HapticFeedback
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.thejohnsondev.ui.components.button.RoundedButton
+import com.thejohnsondev.ui.components.container.RoundedContainer
+import com.thejohnsondev.ui.designsystem.EndRounded
+import com.thejohnsondev.ui.designsystem.Percent100
+import com.thejohnsondev.ui.designsystem.Size12
+import com.thejohnsondev.ui.designsystem.Size4
+import com.thejohnsondev.ui.designsystem.Size8
+import com.thejohnsondev.ui.designsystem.StartRounded
+import com.thejohnsondev.ui.utils.ResString
+import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.annotation.KoinExperimentalAPI
+import vaultmultiplatform.core.ui.generated.resources.password_generator_copy
+import vaultmultiplatform.core.ui.generated.resources.password_generator_generate
 
 @OptIn(KoinExperimentalAPI::class)
 @Composable
@@ -14,18 +44,109 @@ fun PasswordGeneratorWidget(
 ) {
     val state = viewModel.state.collectAsStateWithLifecycle()
 
+    LaunchedEffect(Unit) {
+        viewModel.perform(PasswordGeneratorViewModel.Action.FetchConfig)
+    }
+
     PasswordGeneratorWidgetContent(
+        modifier = modifier,
         state = state.value,
         onAction = viewModel::perform
     )
-
 }
 
 // this should be private, but the previews are in another module rn
 @Composable
 fun PasswordGeneratorWidgetContent(
+    modifier: Modifier = Modifier,
     state: PasswordGeneratorViewModel.State,
     onAction: (PasswordGeneratorViewModel.Action) -> Unit,
 ) {
-    // TODO implement
+    val hapticFeedback = LocalHapticFeedback.current
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        RoundedContainer(
+            modifier = Modifier
+                .padding(Size8)
+                .fillMaxWidth(),
+        ) {
+            Column {
+                MainPasswordView(state)
+                GenerateButtonsRow(hapticFeedback, onAction)
+            }
+        }
+    }
+}
+
+@Composable
+private fun MainPasswordView(state: PasswordGeneratorViewModel.State) {
+    Text(
+        modifier = Modifier
+            .padding(Size8)
+            .fillMaxWidth(),
+        text = buildAnnotatedString {
+            append(state.passwordGeneratedResult?.password.orEmpty())
+        },
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.headlineLarge,
+    )
+    HorizontalDivider(
+        modifier = Modifier
+            .padding(Size4)
+            .fillMaxWidth()
+    )
+    Text(
+        modifier = Modifier
+            .padding(Size8)
+            .fillMaxWidth(),
+        text = buildAnnotatedString {
+            append(state.passwordGeneratedResult?.suggestion.orEmpty())
+        },
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.bodySmall,
+    )
+}
+
+@Composable
+private fun GenerateButtonsRow(
+    hapticFeedback: HapticFeedback,
+    onAction: (PasswordGeneratorViewModel.Action) -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .padding(Size12),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        RoundedButton(
+            modifier = Modifier
+                .padding(end = Size4)
+                .weight(Percent100),
+            text = stringResource(ResString.password_generator_generate),
+            imageVector = Icons.Default.Casino,
+            buttonShape = StartRounded,
+            onClick = {
+                hapticFeedback.performHapticFeedback(
+                    HapticFeedbackType.LongPress
+                )
+                onAction(PasswordGeneratorViewModel.Action.GeneratePassword)
+            }
+        )
+        RoundedButton(
+            modifier = Modifier
+                .padding(start = Size4)
+                .weight(Percent100),
+            text = stringResource(ResString.password_generator_copy),
+            imageVector = Icons.Default.CopyAll,
+            buttonShape = EndRounded,
+            onClick = {
+                hapticFeedback.performHapticFeedback(
+                    HapticFeedbackType.LongPress
+                )
+                onAction(PasswordGeneratorViewModel.Action.Copy)
+            }
+        )
+    }
 }
