95c3b0f48a5113d2d5fb9829b43ac59121dcc72f
Andrew <thejohnsondev@gmail.com>
2025-05-03 12:47:03 +0300

    fix(tools): fix building on desktop and iOS

--- feature/tools/presentation/src/desktopMain/kotlin/com/thejohnsondev/presentation/di/ToolsPresentationModule.desktop.kt
+++ feature/tools/presentation/src/desktopMain/kotlin/com/thejohnsondev/presentation/di/ToolsPresentationModule.desktop.kt
@@ -7,5 +7,5 @@ import org.koin.dsl.module
 
 actual val toolsPresentationModule = module {
     viewModel { PasswordGeneratorViewModel(get(), get(), get(), get()) }
-    viewModel { VaultHealthViewModel(get(), get(), get()) }
+    viewModel { VaultHealthViewModel(get(), get(), get(), get(), get()) }
 }
