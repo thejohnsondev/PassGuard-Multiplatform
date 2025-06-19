ef517d857869cf84f4dffb485bcd23229b850d03
Andrew <thejohnsondev@gmail.com>
2025-06-19 16:50:31 +0300

    feat(autofill): add autofill service

---
+++ feature/autofill/src/main/java/com/thejohnsondev/autofill/VaultAutofillService.kt
@@ -0,0 +1,22 @@
+package com.thejohnsondev.autofill
+
+import android.os.CancellationSignal
+import android.service.autofill.AutofillService
+import android.service.autofill.FillCallback
+import android.service.autofill.FillRequest
+import android.service.autofill.SaveCallback
+import android.service.autofill.SaveRequest
+
+class VaultAutofillService: AutofillService() {
+    override fun onFillRequest(
+        request: FillRequest,
+        cancellationSignal: CancellationSignal,
+        callback: FillCallback
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSaveRequest(request: SaveRequest, callback: SaveCallback) {
+        TODO("Not yet implemented")
+    }
+}
