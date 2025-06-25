0d925be20dd8873331122bb1136006e2983b791f
Andrew <thejohnsondev@gmail.com>
2025-06-25 17:34:26 +0300

    feat(biometric): add biometric login models

--- core/common/src/commonMain/kotlin/com/thejohnsondev/common/utils/BiometricsProvider.kt
+++ core/common/src/commonMain/kotlin/com/thejohnsondev/common/utils/BiometricsProvider.kt
@@ -11,3 +11,33 @@ expect class BiometricsProvider {
         onBiometricError: (String) -> Unit
     )
 }
+
+enum class BiometricType {
+    NONE,
+    FINGERPRINT,
+    FACE,
+    TOUCH_ID,
+    FACE_ID,
+    WINDOWS_HELLO,
+    DEVICE_PASSWORD_FALLBACK
+}
+
+sealed class BiometricAvailability {
+    data class Available(val type: BiometricType) : BiometricAvailability()
+    object NotEnrolled : BiometricAvailability()
+    object HardwareUnavailable : BiometricAvailability()
+    object FeatureNotSupported : BiometricAvailability()
+    object Unknown : BiometricAvailability()
+}
+
+/**
+ * Represents the result of a biometric authentication attempt.
+ */
+sealed class BiometricAuthResult {
+    object Success : BiometricAuthResult()
+    object UserCancelled : BiometricAuthResult()
+    object Failed : BiometricAuthResult()
+    object LockedOut : BiometricAuthResult()
+    data class Error(val code: Int, val message: String) : BiometricAuthResult()
+    object NotAvailable : BiometricAuthResult()
+}
