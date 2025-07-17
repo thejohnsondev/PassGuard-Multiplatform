0c8e0e0e7916b1267db5a94c7d5b16924fa573a3
Andrew <thejohnsondev@gmail.com>
2025-07-17 17:23:47 +0300

    refactor(general): clean up the :core:datastore module

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -8,7 +7,6 @@ plugins {
 
 kotlin {
     androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
