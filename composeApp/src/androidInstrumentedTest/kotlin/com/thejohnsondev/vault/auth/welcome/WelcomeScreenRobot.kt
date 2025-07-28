74859da1a8cf2db66f3bccdd8e090e43d8b26187
Andrew <thejohnsondev@gmail.com>
2025-07-28 16:47:25 +0300

    feat(test): add assertChooseVaultTypeIsDisplayed

--- composeApp/src/androidInstrumentedTest/kotlin/com/thejohnsondev/vault/auth/welcome/WelcomeScreenRobot.kt
+++ composeApp/src/androidInstrumentedTest/kotlin/com/thejohnsondev/vault/auth/welcome/WelcomeScreenRobot.kt
@@ -3,6 +3,7 @@ package com.thejohnsondev.vault.auth.welcome
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.ComposeTestRule
 import androidx.compose.ui.test.performClick
 
@@ -10,6 +11,8 @@ class WelcomeScreenRobot(private val composeTestRule: ComposeTestRule) {
 
     private val getStartedButtonMatcher =
         hasContentDescription("Get Started")
+    private val chooseVaultTypeTextMatcher =
+        hasText("Choose your vault type")
 
     @OptIn(ExperimentalTestApi::class)
     fun clickGetStartedButton() {
@@ -30,6 +33,17 @@ class WelcomeScreenRobot(private val composeTestRule: ComposeTestRule) {
         )
         composeTestRule
             .onNode(getStartedButtonMatcher)
+            .assertIsDisplayed()
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    fun assertChooseVaultTypeIsDisplayed() {
+        composeTestRule.waitUntilAtLeastOneExists(
+            chooseVaultTypeTextMatcher,
+            timeoutMillis = 5000
+        )
+        composeTestRule
+            .onNode(chooseVaultTypeTextMatcher)
             .assertIsDisplayed()
     }
 }
