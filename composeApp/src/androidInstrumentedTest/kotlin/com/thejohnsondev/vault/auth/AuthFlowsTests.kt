639966b20e45c87c02e803508fd713bf2632cf93
Andrew <thejohnsondev@gmail.com>
2025-07-30 16:43:47 +0300

    feat(test): add AuthFlowsTests, add empty SelectVaultTypeRobot

---
+++ composeApp/src/androidInstrumentedTest/kotlin/com/thejohnsondev/vault/auth/AuthFlowsTests.kt
@@ -0,0 +1,27 @@
+package com.thejohnsondev.vault.auth
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.thejohnsondev.vault.auth.selectvaulttype.SelectVaultTypeRobot
+import com.thejohnsondev.vault.auth.welcome.WelcomeScreenRobot
+import org.junit.Rule
+import org.junit.Test
+import org.thejohnsondev.vault.MainActivity
+
+class AuthFlowsTests {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun testLocalVaultCreation() {
+        with(WelcomeScreenRobot(composeTestRule)) {
+            assertGetStartedButtonIsDisplayed()
+            clickGetStartedButton()
+            assertChooseVaultTypeIsDisplayed()
+        }
+        with(SelectVaultTypeRobot(composeTestRule)) {
+            assertOptionVisible()
+            clickLocalVaultOption()
+        }
+    }
+}
---
+++ composeApp/src/androidInstrumentedTest/kotlin/com/thejohnsondev/vault/auth/selectvaulttype/SelectVaultTypeRobot.kt
@@ -0,0 +1,14 @@
+package com.thejohnsondev.vault.auth.selectvaulttype
+
+import androidx.compose.ui.test.junit4.ComposeTestRule
+
+class SelectVaultTypeRobot(private val composeTestRule: ComposeTestRule) {
+
+    fun assertOptionVisible() {
+        // TODO implement
+    }
+
+    fun clickLocalVaultOption() {
+        // TODO implement
+    }
+}
