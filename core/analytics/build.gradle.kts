2e13ac85481f6e920763f918bee9d52bba581efa
Andrew <thejohnsondev@gmail.com>
2025-08-23 16:58:49 +0300

    feat(analytics): add analytics module

---
+++ core/analytics/build.gradle.kts
@@ -0,0 +1,63 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinSerialization)
+}
+
+kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+
+    macosX64()
+    macosArm64()
+
+    jvm("desktop")
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        androidMain.dependencies {
+
+        }
+        commonMain.dependencies {
+            implementation(libs.ktor.serialization.kotlinx.json)
+
+            // Koin
+            api(libs.koin.core)
+        }
+    }
+}
+
+android {
+    namespace = "org.thejohnsondev.analytics"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    lint {
+        disable.add("NullSafeMutableLiveData")
+    }
+}
+
+dependencies {
+    androidTestImplementation(libs.runner)
+}
