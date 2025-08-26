047027c6e0ea78a52b1989ea1e163efe41cc0f0b
Andrew <thejohnsondev@gmail.com>
2025-08-26 11:03:19 +0300

    feat(analytics): add ios implementation of PostHog analytics functions

--- iosApp/iosApp/analytics/PosthogAnalyticsPlatformImpl.swift
+++ iosApp/iosApp/analytics/PosthogAnalyticsPlatformImpl.swift
@@ -1,15 +1,18 @@
 import Foundation
 import ComposeApp
+import PostHog
 
 
 class PosthogAnalyticsPlatformImpl: AnalyticsPlatform {
    
     func doInitPlatform(config: AnalyticsConfig) {
-        
+        let platformConfig: PosthogAnalyticsConfig = config as! PosthogAnalyticsConfig
+        let config = PostHogConfig(apiKey: platformConfig.apiKey, host: platformConfig.host)
+        PostHogSDK.shared.setup(config)
     }
     
     func trackEventPlatform(name: String, props: [String: Any]) {
-        
+        PostHogSDK.shared.capture(name, properties: props)
     }
     
     func logCrashPlatform(t: KotlinThrowable) {
