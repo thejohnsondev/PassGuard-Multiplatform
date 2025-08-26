f734c7ef3ee396dd8b1354f2182a470daafb98a7
Andrew <thejohnsondev@gmail.com>
2025-08-26 11:19:30 +0300

    feat(analytics): fix analytics events sending fro android and iOS

--- core/analytics/src/androidMain/kotlin/com/thejohnsondev/analytics/posthog/AndroidPosthogAnalyticsPlatform.kt
+++ core/analytics/src/androidMain/kotlin/com/thejohnsondev/analytics/posthog/AndroidPosthogAnalyticsPlatform.kt
@@ -6,31 +6,24 @@ import com.posthog.android.PostHogAndroid
 import com.posthog.android.PostHogAndroidConfig
 import com.thejohnsondev.analytics.AnalyticsConfig
 import com.thejohnsondev.analytics.AnalyticsPlatform
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class AndroidPosthogAnalyticsPlatform(private val context: Context) : AnalyticsPlatform {
-    private val scope = CoroutineScope(Dispatchers.IO)
 
     override fun initPlatform(config: AnalyticsConfig) {
-        scope.launch {
-            val posthogConfig = config as PosthogAnalyticsConfig
-            val nativeConfig = PostHogAndroidConfig(
-                apiKey = posthogConfig.apiKey,
-                host = posthogConfig.host
-            )
-            PostHogAndroid.setup(context, nativeConfig)
-        }
+        val posthogConfig = config as PosthogAnalyticsConfig
+        val nativeConfig = PostHogAndroidConfig(
+            apiKey = posthogConfig.apiKey,
+            host = posthogConfig.host
+        )
+        PostHogAndroid.setup(context, nativeConfig)
+
     }
 
     override fun trackEventPlatform(
         name: String,
         props: Map<String, Any>
     ) {
-        scope.launch {
-            PostHog.capture(event = name, properties = props)
-        }
+        PostHog.capture(event = name, properties = props)
     }
 
     override fun logCrashPlatform(t: Throwable) {
--- core/analytics/src/desktopMain/kotlin/com/thejohnsondev/analytics/posthog/DesktopPosthogAnalyticsPlatform.kt
+++ core/analytics/src/desktopMain/kotlin/com/thejohnsondev/analytics/posthog/DesktopPosthogAnalyticsPlatform.kt
@@ -3,27 +3,25 @@ package com.thejohnsondev.analytics.posthog
 import com.posthog.java.PostHog
 import com.thejohnsondev.analytics.AnalyticsConfig
 import com.thejohnsondev.analytics.AnalyticsPlatform
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 class DesktopPosthogAnalyticsPlatform : AnalyticsPlatform {
 
-    private val scope = CoroutineScope(Dispatchers.IO)
+    private var postHog: PostHog? = null
 
     override fun initPlatform(config: AnalyticsConfig) {
-        scope.launch {
-            val posthogConfig = config as PosthogAnalyticsConfig
-            PostHog.Builder(posthogConfig.apiKey).host(posthogConfig.host).build()
-        }
+        val posthogConfig = config as PosthogAnalyticsConfig
+        postHog = PostHog.Builder(posthogConfig.apiKey).host(posthogConfig.host).build()
     }
 
+    @OptIn(ExperimentalUuidApi::class)
     override fun trackEventPlatform(
         name: String,
         props: Map<String, Any>
     ) {
-
-        // TODO implement
+        val distinctID = Uuid.random().toString() // TODO replace with actual distinct ID logic
+        postHog?.capture(distinctID, name, props)
     }
 
     override fun logCrashPlatform(t: Throwable) {
