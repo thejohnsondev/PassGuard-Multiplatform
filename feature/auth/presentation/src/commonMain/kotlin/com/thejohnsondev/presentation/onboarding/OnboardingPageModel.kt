3162343898e614b4ee7fdaf9a38dd4f931dde3ac
Andrew <thejohnsondev@gmail.com>
2025-09-06 09:58:24 +0300

    feat(onboarding): add showing onboarding screens text

--- feature/auth/presentation/src/commonMain/kotlin/com/thejohnsondev/presentation/onboarding/OnboardingPageModel.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/thejohnsondev/presentation/onboarding/OnboardingPageModel.kt
@@ -1,16 +1,37 @@
 package com.thejohnsondev.presentation.onboarding
 
+import com.thejohnsondev.ui.utils.ResString
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.StringResource
+import vaultmultiplatform.core.ui.generated.resources.onboarding_export_title
+import vaultmultiplatform.core.ui.generated.resources.onboarding_offline_title
+import vaultmultiplatform.core.ui.generated.resources.onboarding_security_description
+import vaultmultiplatform.core.ui.generated.resources.onboarding_security_title
 
 data class OnboardingPageModel(
     val titleStringRes: StringResource,
     val descriptionStringRes: StringResource,
-    val imageRes: DrawableResource,
+    val imageRes: DrawableResource?,
 ) {
 
     companion object {
-        val pages = emptyList<OnboardingPageModel>()
+        val pages = listOf(
+            OnboardingPageModel(
+                titleStringRes = ResString.onboarding_security_title,
+                descriptionStringRes = ResString.onboarding_security_description,
+                imageRes = null
+            ),
+            OnboardingPageModel(
+                titleStringRes = ResString.onboarding_offline_title,
+                descriptionStringRes = ResString.onboarding_security_description,
+                imageRes = null
+            ),
+            OnboardingPageModel(
+                titleStringRes = ResString.onboarding_export_title,
+                descriptionStringRes = ResString.onboarding_security_description,
+                imageRes = null
+            )
+        )
     }
 
 }
--- feature/auth/presentation/src/commonMain/kotlin/com/thejohnsondev/presentation/onboarding/OnboardingScreen.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/thejohnsondev/presentation/onboarding/OnboardingScreen.kt
@@ -1,29 +1,45 @@
 package com.thejohnsondev.presentation.onboarding
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.thejohnsondev.ui.designsystem.EquallyRounded
 import com.thejohnsondev.ui.designsystem.Percent100
 import com.thejohnsondev.ui.designsystem.Size16
 import com.thejohnsondev.ui.designsystem.Size20
 import com.thejohnsondev.ui.designsystem.Size360
 import com.thejohnsondev.ui.designsystem.Size8
+import com.thejohnsondev.ui.designsystem.SizeBorder
+import com.thejohnsondev.ui.utils.ResString
 import com.thejohnsondev.ui.utils.padding
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.resources.vectorResource
+import vaultmultiplatform.core.ui.generated.resources.btn_next
+import vaultmultiplatform.core.ui.generated.resources.onboarding_create_vault
 
 @Composable
 fun OnboardingScreen(
@@ -34,20 +50,73 @@ fun OnboardingScreen(
     val pages = remember {
         OnboardingPageModel.pages
     }
+    val coroutineScope = rememberCoroutineScope()
     val pagerState = rememberPagerState(initialPage = 0, pageCount = { pages.size })
     Surface {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(Size16)
+                .padding(Size16),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
             HorizontalPager(
                 state = pagerState,
                 modifier = Modifier
                     .weight(Percent100)
-                    .fillMaxWidth()
+                    .fillMaxWidth(),
+                snapPosition = SnapPosition.Center,
+                verticalAlignment = Alignment.CenterVertically
             ) { page ->
                 OnBoardItem(pages[page])
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                val isLastPage = pagerState.currentPage == pages.size - 1
+                Button(
+                    modifier = Modifier.align(Alignment.CenterEnd),
+                    content = {
+                        Text(
+                            text = stringResource(
+                                if (isLastPage) ResString.onboarding_create_vault else ResString.btn_next
+                            )
+                        )
+                    },
+                    onClick = {
+                        if (!isLastPage) {
+                            val nextPage = pagerState.currentPage + 1
+                            coroutineScope.launch { pagerState.animateScrollToPage(nextPage) }
+                        }
+                    }
+                )
+                Row(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(bottom = Size20),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    repeat(pages.size) { index ->
+                        val isSelected = pagerState.currentPage == index
+                        Box(
+                            modifier = Modifier
+                                .padding(4.dp)
+                                .width(if (isSelected) Size20 else Size8)
+                                .height(if (isSelected) Size16 else Size8)
+                                .border(
+                                    width = SizeBorder,
+                                    color = MaterialTheme.colorScheme.onPrimary,
+                                    shape = EquallyRounded.medium
+                                )
+                                .background(
+                                    color = if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onPrimary,
+                                    shape = EquallyRounded.extraLarge
+                                )
+                        )
+                    }
+                }
             }
         }
     }
@@ -60,13 +129,15 @@ fun OnBoardItem(page: OnboardingPageModel) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Image(
-            imageVector = vectorResource(page.imageRes),
-            contentDescription = null,
-            modifier = Modifier
-                .size(Size360)
-                .padding(bottom = Size20)
-        )
+        page.imageRes?.let {
+            Image(
+                imageVector = vectorResource(page.imageRes),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(Size360)
+                    .padding(bottom = Size20)
+            )
+        }
         Text(
             text = stringResource(page.titleStringRes),
             style = MaterialTheme.typography.titleLarge
