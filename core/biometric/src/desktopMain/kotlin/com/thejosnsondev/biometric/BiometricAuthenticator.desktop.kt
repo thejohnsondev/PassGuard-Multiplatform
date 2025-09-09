5f0cfed8b088325c4e8b6ec138a3b4976f54b30e
Andrew <thejohnsondev@gmail.com>
2025-09-09 16:33:20 +0300

    feat(mac): change machelper executable location

--- core/biometric/src/desktopMain/kotlin/com/thejosnsondev/biometric/BiometricAuthenticator.desktop.kt
+++ core/biometric/src/desktopMain/kotlin/com/thejosnsondev/biometric/BiometricAuthenticator.desktop.kt
@@ -6,6 +6,7 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import java.io.File
 import java.io.IOException
+import java.nio.file.Files
 import java.nio.file.Paths
 
 @Serializable
@@ -20,7 +21,7 @@ data class AuthResultResponse(
 
 actual class BiometricAuthenticator {
 
-    private val helperExecutableName = "PassGuard MacOS Biometric Check.kexe"
+    private val helperExecutableName = "PassGuardMacOSBiometricCheck.kexe"
     private val json = Json { ignoreUnknownKeys = true }
 
 
@@ -30,35 +31,10 @@ actual class BiometricAuthenticator {
             return null
         }
 
-        val projectRoot = System.getProperty("user.dir")
-
-        val helperDevPathArm64 = Paths.get(
-            projectRoot,
-            "../utils",
-            "machelper",
-            "build",
-            "bin",
-            "macosArm64",
-            "releaseExecutable",
-            helperExecutableName
-        ).toString()
-        if (File(helperDevPathArm64).exists()) {
-            println("Found helper at development path (ARM64): $helperDevPathArm64")
-            return helperDevPathArm64
-        }
-
-        val helperDevPathX64 = Paths.get(
-            projectRoot,
-            "machelper",
-            "build",
-            "bin",
-            "macosX64",
-            "releaseExecutable",
-            helperExecutableName
-        ).toString()
-        if (File(helperDevPathX64).exists()) {
-            println("Found helper at development path (X64): $helperDevPathX64")
-            return helperDevPathX64
+        val helperDevPath = extractHelperExecutable()
+        if (File(helperDevPath).exists()) {
+            println("Found helper at development path: $helperDevPath")
+            return helperDevPath
         }
 
         val currentAppMacOSDir =
@@ -73,6 +49,27 @@ actual class BiometricAuthenticator {
 
         println("macOSBiometricHelper executable ($helperExecutableName) not found at expected locations for current OS ($osName).")
         return null
+    }
+
+
+    private fun extractHelperExecutable(): String {
+        val resourceName = "PassGuardMacOSBiometricCheck.kexe"
+
+        val resource = Thread.currentThread().contextClassLoader.getResourceAsStream(resourceName)
+            ?: error("Resource $resourceName not found")
+        val tempDir = Files.createTempDirectory("biometric-helper").toFile()
+        tempDir.deleteOnExit()
+        val tempFile = File(tempDir, resourceName)
+        tempFile.deleteOnExit()
+
+        resource.use { input ->
+            tempFile.outputStream().use { output ->
+                input.copyTo(output)
+            }
+        }
+        tempFile.setExecutable(true)
+
+        return tempFile.absolutePath
     }
 
     actual fun getBiometricAvailability(): BiometricAvailability {
