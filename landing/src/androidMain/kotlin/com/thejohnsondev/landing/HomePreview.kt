1baa2cdece31155e07cd7c1fc41f753e32c44874
Andrew <thejohnsondev@gmail.com>
2025-11-01 16:39:51 +0200

    feat(landing): remove extra previews

--- landing/src/androidMain/kotlin/com/thejohnsondev/landing/HomePreview.kt
+++ landing/src/androidMain/kotlin/com/thejohnsondev/landing/HomePreview.kt
@@ -1,84 +1,25 @@
 package com.thejohnsondev.landing
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.thejohnsondev.landing.components.WebNavBar
+import com.thejohnsondev.landing.components.WebAppContainer
 import com.thejohnsondev.landing.components.WebScaffold
 import com.thejohnsondev.landing.home.HomeScreen
 
 @Preview(widthDp = 1280)
 @Composable
-private fun NavBarNotCollapsedPreview() {
-    PreviewTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(MaterialTheme.colorScheme.surface)
-        ) {
-            WebNavBar(
-                modifier = Modifier
-                    .align(Alignment.Center),
-                navigateTo = {
-
-                },
-                isCollapsed = false,
-            )
-        }
-    }
-}
-
-@Preview(widthDp = 1280)
-@Composable
-private fun NavBarCollapsedPreview() {
-    PreviewTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(MaterialTheme.colorScheme.surface)
-        ) {
-            WebNavBar(
-                modifier = Modifier
-                    .align(Alignment.Center),
-                navigateTo = {
-
-                },
-                isCollapsed = true,
-            )
-        }
-    }
-}
-
-@Preview(widthDp = 1280)
-@Composable
 private fun HomeScreenPreview() {
-    PreviewTheme {
+    PreviewTheme(
+        darkTheme = false
+    ) {
         WebScaffold(
             modifier = Modifier
                 .fillMaxWidth(),
             navigateTo = {},
             webAppContainer = WebAppContainer.State(),
             onAction = {},
-        ) { paddings ->
-            HomeScreen(paddings)
-        }
-    }
-}
-
-@Preview(widthDp = 1280)
-@Composable
-private fun HomeScreenDarkPreview() {
-    PreviewTheme(
-        darkTheme = true
-    ) {
-        WebScaffold(
-            modifier = Modifier
-                .fillMaxWidth()
         ) { paddings ->
             HomeScreen(paddings)
         }
