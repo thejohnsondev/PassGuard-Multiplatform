e9e5139493ee65fa65f2a8a88d0aee6ccd3d66be
Andrew <thejohnsondev@gmail.com>
2025-11-01 12:02:27 +0200

    feat(landing): apply AnimatedAppear to separate parts of title

--- landing/src/commonMain/kotlin/com/thejohnsondev/landing/home/HomeScreen.kt
+++ landing/src/commonMain/kotlin/com/thejohnsondev/landing/home/HomeScreen.kt
@@ -1,8 +1,5 @@
 package com.thejohnsondev.landing.home
 
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.AnimationVector1D
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -19,47 +16,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.blur
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.thejohnsondev.ui.components.VaultLogo
+import com.thejohnsondev.ui.components.animation.appear.AnimatedAppear
+import com.thejohnsondev.ui.components.animation.appear.AnimatedAppearParams
 import com.thejohnsondev.ui.designsystem.Size12
 import com.thejohnsondev.ui.designsystem.Size16
 import com.thejohnsondev.ui.designsystem.Size64
 import com.thejohnsondev.ui.designsystem.Size80
 import com.thejohnsondev.ui.utils.ResDrawable
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
 import vaultmultiplatform.core.ui.generated.resources.ic_vault_108_gradient
-
-private const val DELAY_BEFORE_ANIM = 200L
-
-private const val BLUR_SCALE_ANIM_START = 20f
-private const val BLUR_SCALE_ANIM_END = 0f
-private const val BLUR_SCALE_ANIM_DURATION = 600
-
-private const val LOGO_Y_ANIM_START = -100f
-private const val LOGO_Y_ANIM_END_PX = 0f
-private const val LOGO_Y_ANIM_END_DURATION = 700
-
-private const val LOGO_SCALE_ANIM_START = 0.9f
-private const val LOGO_SCALE_ANIM_END = 1f
-private const val LOGO_SCALE_ANIM_DURATION = 400
-
-private const val LOGO_ALPHA_ANIM_START = 0f
-private const val LOGO_ALPHA_ANIM_END = 1f
-private const val LOGO_ALPHA_ANIM_DURATION = 300
 
 @Composable
 fun HomeScreen(
@@ -84,52 +56,46 @@ fun HomeScreen(
 
 @Composable
 private fun TitleContent() {
-    val animatedLogoBlurScale = remember {
-        Animatable(BLUR_SCALE_ANIM_START)
-    }
-    val animatedLogoScale = remember {
-        Animatable(LOGO_SCALE_ANIM_START)
-    }
-    val animatedLogoAlpha = remember {
-        Animatable(LOGO_ALPHA_ANIM_START)
-    }
-    val animatedLogoYPosition = remember {
-        Animatable(LOGO_Y_ANIM_START)
-    }
-    val animatedLogoYPosEnd = 0 - with(LocalDensity.current) {
-        LOGO_Y_ANIM_END_PX.dp.toPx()
-    }
-
-    LaunchedEffect(true) {
-        startAnimations(
-            this,
-            animatedLogoBlurScale,
-            animatedLogoScale,
-            animatedLogoAlpha,
-            animatedLogoYPosition,
-            animatedLogoYPosEnd
-        )
-    }
-
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .blur(animatedLogoBlurScale.value.dp)
-            .alpha(animatedLogoAlpha.value)
-            .graphicsLayer {
-                translationY = animatedLogoYPosition.value
-            },
+            .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        ContentLogo(
+        AnimatedAppear { animatedModifier ->
+            ContentLogo(
+                modifier = animatedModifier
+                    .padding(top = Size64)
+            )
+        }
+        AnimatedAppear(
+            params = AnimatedAppearParams.default(
+                delayBeforeAnim = 400L
+            )
+        ) { animatedModifier ->
+            ContentDescription(
+                modifier = animatedModifier
+                    .padding(top = Size64)
+            )
+        }
+        Box(
             modifier = Modifier
-
-                .padding(top = Size64)
+                .fillMaxWidth()
+                .height(1000.dp)
         )
+    }
+}
+
+@Composable
+private fun ContentDescription(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
         Text(
-            modifier = Modifier
-                .padding(top = Size64),
             text = "A simple, secure and easy to use \npassword manager.", // todo anotated string with accent on simple secure and easy
             style = MaterialTheme.typography.headlineMedium,
             fontSize = 42.sp,
@@ -147,17 +113,12 @@ private fun TitleContent() {
             textAlign = TextAlign.Center,
             fontWeight = FontWeight.Normal
         )
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(1000.dp)
-        )
     }
 }
 
 @Composable
 private fun ContentLogo(
-    modifier: Modifier
+    modifier: Modifier = Modifier
 ) {
     Row(
         modifier = modifier,
@@ -172,41 +133,6 @@ private fun ContentLogo(
         VaultLogo(
             modifier = Modifier
                 .padding(horizontal = Size16)
-        )
-    }
-}
-
-private suspend fun startAnimations(
-    coroutineScope: CoroutineScope,
-    animatedBackgroundBlurScale: Animatable<Float, AnimationVector1D>,
-    animatedLogoScale: Animatable<Float, AnimationVector1D>,
-    animatedLogoAlpha: Animatable<Float, AnimationVector1D>,
-    animatedLogoYPosition: Animatable<Float, AnimationVector1D>,
-    animatedLogoYPosEnd: Float,
-) {
-    delay(DELAY_BEFORE_ANIM)
-    coroutineScope.launch {
-        animatedBackgroundBlurScale.animateTo(
-            targetValue = BLUR_SCALE_ANIM_END,
-            animationSpec = tween(durationMillis = BLUR_SCALE_ANIM_DURATION)
-        )
-    }
-    coroutineScope.launch {
-        animatedLogoScale.animateTo(
-            targetValue = LOGO_SCALE_ANIM_END,
-            animationSpec = tween(durationMillis = LOGO_SCALE_ANIM_DURATION)
-        )
-    }
-    coroutineScope.launch {
-        animatedLogoAlpha.animateTo(
-            targetValue = LOGO_ALPHA_ANIM_END,
-            animationSpec = tween(durationMillis = LOGO_ALPHA_ANIM_DURATION)
-        )
-    }
-    coroutineScope.launch {
-        animatedLogoYPosition.animateTo(
-            targetValue = animatedLogoYPosEnd,
-            animationSpec = tween(durationMillis = LOGO_Y_ANIM_END_DURATION)
         )
     }
 }
