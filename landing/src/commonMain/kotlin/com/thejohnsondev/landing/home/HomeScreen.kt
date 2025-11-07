06d40fcce853822f8ee301645771e81196684588
Andrew <thejohnsondev@gmail.com>
2025-11-07 17:57:34 +0200

    feat(landing): move phone box out of scroll

--- landing/src/commonMain/kotlin/com/thejohnsondev/landing/home/HomeScreen.kt
+++ landing/src/commonMain/kotlin/com/thejohnsondev/landing/home/HomeScreen.kt
@@ -16,23 +16,17 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.LayoutCoordinates
-import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -46,7 +40,6 @@ import com.thejohnsondev.ui.components.animation.appear.AnimatedAppearParams
 import com.thejohnsondev.ui.designsystem.Size12
 import com.thejohnsondev.ui.designsystem.Size128
 import com.thejohnsondev.ui.designsystem.Size16
-import com.thejohnsondev.ui.designsystem.Size2
 import com.thejohnsondev.ui.designsystem.Size64
 import com.thejohnsondev.ui.designsystem.Size80
 import com.thejohnsondev.ui.utils.ResDrawable
@@ -78,43 +71,27 @@ fun HomeScreen(
     val windowHeightPx = with(density) { windowHeight.dp.toPx() }
     val middleOfWindow = windowHeightPx / 2f
 
-    val centerY = phoneBoxCoordinates?.positionInWindow()?.y?.plus(
-        phoneBoxCoordinates?.size?.height?.div(2f) ?: 0f
-    ) ?: 0f
+    Logger.d("middleOfWindow: $middleOfWindow, scrollY: ${scrollState.value}")
 
-    val tolerancePx = 200
-
-    val isPhoneBoxInCenter = centerY <= middleOfWindow && centerY >= middleOfWindow - tolerancePx
-
-    Surface(
-        modifier = Modifier
-            .verticalScroll(scrollState)
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(paddingValues),
-        color = MaterialTheme.colorScheme.surface
-    ) {
+    Box {
         Box(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .verticalScroll(scrollState)
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(paddingValues)
+                .background(MaterialTheme.colorScheme.surface),
         ) {
-            HorizontalDivider(
-                modifier = Modifier.height(Size2).fillMaxWidth().graphicsLayer {
-                    translationY = centerY
-                }, color = Color.Red
-            )
-            HorizontalDivider(
-                modifier = Modifier.height(Size2).fillMaxWidth().graphicsLayer {
-                    translationY = middleOfWindow
-                }, color = Color.Green
-            )
             Background()
             Content()
-            PhoneBoxContent(
-                modifier = Modifier
-                    .padding(top = Size64)
-
-            )
         }
+        PhoneBoxContent(
+            modifier = Modifier
+                .graphicsLayer {
+                    translationY = (middleOfWindow - 200f) - scrollState.value.toFloat()
+                }
+
+        )
     }
 }
 
